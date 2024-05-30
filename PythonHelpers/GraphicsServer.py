4e6ac896f67db99e27f90d4d705e8b28fa86c6d9
jimdi <powersaiden07@gmail.com>
2024-05-30 10:38:10 -0700

    Edited webhooks.

--- PythonHelpers/GraphicsServer.py
+++ PythonHelpers/GraphicsServer.py
@@ -1,3 +1,4 @@
+import requests
 from flask import Flask, jsonify
 from flask_socketio import SocketIO, emit
 from flask_cors import CORS
@@ -13,20 +14,58 @@ VolPosition = "VolPosition"
 VolName = "VolName"
 VolTeamNum = "VolTeamNum"
 
+names = {
+    "1": "Volunteer Name 1",
+    "2": "Volunteer Name 2",
+    "3": "Volunteer Name 3",
+    "4": "Volunteer Name 4",
+    "5": "Volunteer Name 5"
+}
+
+position = {
+    "1": "Volunteer Position 1",
+    "2": "Volunteer Position 2",
+    "3": "Volunteer Position 3",
+    "4": "Volunteer Position 4",
+    "5": "Volunteer Position 5"
+}
+
+teams = {
+    "1": "Team Number 1",
+    "2": "Team Number 2",
+    "3": "Team Number 3",
+    "4": "Team Number 4",
+    "5": "Team Number 5"
+}
+
+currentperson = 1
+
+
+def getPerson():
+    global currentperson
+    currentperson += 1
+    if currentperson > names.__len__():
+        currentperson = 1
+    return position[str(currentperson)], names[str(currentperson)], teams[str(currentperson)]
+
 
 def background_thread():  # test function that updates the text every ms
     global counter
+    global VolPosition, VolName, VolTeamNum
     while True:
         #socketio.sleep(0.001) #1ms
-        socketio.sleep(1)
+        socketio.sleep(1 / 30)
         counter += 1
-        socketio.emit('active_message', {'message': 'Webhook Updates: ' + str(counter) + ' Server Interval: 1s'},
+        socketio.emit('active_message', {'message': 'Webhook Updates: ' + str(counter) + ' Server Interval: 30tps'},
                       namespace='/GetActiveMessage')
+        if counter % 40 == 0:
+            VolPosition, VolName, VolTeamNum = getPerson()
+            socketio.emit('active_vols', {'VolPosition': VolName, 'VolName': VolPosition, 'VolTeamNum': VolTeamNum},
+                          namespace='/GetActiveVols')
 
 
 @socketio.on('connect', namespace='/GetActiveMessage')
 def get_active_message():
-    print("Client connected")
     emit('active_message', {'message': active_message})
 
 
@@ -35,7 +74,6 @@ def update_message_call(msg):
     global active_message
     active_message = msg
     socketio.emit('active_message', {'message': active_message}, namespace='/GetActiveMessage')
-    print("Message updated: " + active_message)
     return jsonify({'result': 'Message updated', 'message': active_message})
 
 
@@ -47,14 +85,12 @@ def update_vol_position_call(pos, name, team):
     VolTeamNum = team
     socketio.emit('active_vols', {'VolPosition': VolPosition, 'VolName': VolName, 'VolTeamNum': VolTeamNum},
                   namespace='/GetActiveVols')
-    print("Volunteer Position updated: " + VolPosition + " " + VolName + " " + VolTeamNum)
     return jsonify({'result': 'Volunteer Position updated', 'VolPosition': VolPosition, 'VolName': VolName,
                     'VolTeamNum': VolTeamNum})
 
 
 @socketio.on('connect', namespace='/GetActiveVols')
 def get_active_vols():
-    print("Client connected")
     emit('active_vols', {'VolPosition': VolPosition, 'VolName': VolName, 'VolTeamNum': VolTeamNum})
 
 
